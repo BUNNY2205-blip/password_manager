1aa93e59ac02fddd99446caf8790b021f2822cd6
Sanchit Sharma <sanchitsharma2205@gmail.com>
2025-07-03 17:50:57 +0530

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -1,17 +1,24 @@
+import os
 from cryptography.fernet import Fernet
-#loading the secret key into the file
+
+KEY_FILE = "secret.key"
+
+def generate_key():
+    key = Fernet.generate_key()
+    with open(KEY_FILE, "wb") as key_file:
+        key_file.write(key)
+
 def load_key():
-    return open('secret.key','rb').read()
-#now we will be encrypting the password string using fernet
+    if not os.path.exists(KEY_FILE):
+        generate_key()
+    return open(KEY_FILE, "rb").read()
+
 def encrypt_passw(password):
     key = load_key()
-    fernet = Fernet(key)
-    encryption = fernet.encrypt(password.encode())
-    return encryption.decode()
-#converting the bytes to string for storage
+    f = Fernet(key)
+    return f.encrypt(password.encode()).decode()
+
 def decrypt_passw(encrypted_password):
     key = load_key()
-    fernet = Fernet(key)
-    decryption = fernet.decrypt(encrypted_password.encode())
-    return decryption.decode()
-#converts bytes back to string
+    f = Fernet(key)
+    return f.decrypt(encrypted_password.encode()).decode()
